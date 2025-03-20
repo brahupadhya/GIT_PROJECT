9b791ccad3a61c336be093efc4b852b4ec50b8c0
BRAHUPADHYA <86275246+brahupadhya@users.noreply.github.com>
2025-03-20 22:50:50 +0800

    200325

--- Summary/venv/Scripts/activate.bat
+++ Summary/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\My Drive\PENELITIAN DIPA\PENELITIAN 2025\Aplikasi\PaythonProject\PaythonProject\venv
+set VIRTUAL_ENV=D:\GITHUB\GIT_PROJECT\Summary\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
